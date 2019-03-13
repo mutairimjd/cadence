7d32debf481d2c04adbad347111753d070190196
AhmadG1 <ahmad.sr.yaseen@gmail.com>
2019-03-13 14:45:43 +0300

    refactor after bad rebacing

--- config/routes.rb
+++ config/routes.rb
@@ -3,26 +3,14 @@ Rails.application.routes.draw do
   get "songs/user/favorites", to:"songs#user_fav_songs", as: "user_fav_songs"
   get "songs/user/", to:"songs#user_songs", as: "user_songs"
   devise_for :users
-  resources :songs
-  # root 'songs#index'
-  get "/pages/:page" => "pages#show"
-  root "pages#show", page: "landing"
 
   resources :songs do
     resources :comments
-  resources :songs do 
     resources :likes
-
   end
-
-
-
-  get "songs/catogrie/:catogrie_id", to:"songs#index", as: "catogrie"
 
   get "songs/catogrie/:catogrie_id", to:"songs#catogrie_songs", as: "catogrie"
   
-
-  root 'songs#index'
   # root 'songs#index'
   get "/pages/:page" => "pages#show"
   root "pages#show", page: "landing"
