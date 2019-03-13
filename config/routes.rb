c29fab615fa13e86e216b41c445fb46e768ec0d5
AhmadG1 <ahmad.sr.yaseen@gmail.com>
2019-03-13 11:32:48 +0300

    ahmad styles updates inprogress

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :songs
-  root 'songs#index'
+  # root 'songs#index'
+  get "/pages/:page" => "pages#show"
+  root "pages#show", page: "landing"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
